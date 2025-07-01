a39352f23faf0ff8989507c165023ebd8d48e22b
Varun Narravula <varun@snare.dev>
2025-07-01 00:14:55 -0700

    feat(module): add home-manager convenience module doc-gen function

--- nix/lib.nix
+++ nix/lib.nix
@@ -26,17 +26,19 @@
 
   @param  options   options attribute set to generate options list from
   @param  excluded  paths to options or sets of options to skip over
+  @param  transform function to apply to each option
   @return           a derivation that builds an options.json file
   */
   mkOptionsList = {
     options,
     pkgs,
+    transform ? lib.id,
     excluded ? [],
   }: let
     options' = removeAtPath excluded options;
 
     rawOptions =
-      lib.optionAttrSetToDocList options';
+      map transform (lib.optionAttrSetToDocList options');
     filteredOptions = lib.filter (opt: opt.visible && !opt.internal) rawOptions;
 
     optionsJSON = builtins.unsafeDiscardStringContext (builtins.toJSON filteredOptions);
@@ -44,7 +46,7 @@
     pkgs.writeText "options.json" optionsJSON;
 
   /*
-  Create an options list JSON string from a list of modules.
+  Create an options list JSON file from a list of modules.
 
   @param  modules   list of modules containing options
   @return           a derivation that builds an options.json file
@@ -65,11 +67,78 @@
       inherit pkgs;
       inherit (eval'd) options;
     };
+
+  hm = {
+    /*
+    Create an options list JSON file from a Home Manager source list of modules.
+
+    @param  home-manager  path to a home-manager source (like from a flake input or tarball)
+    @param  modules       list of extra modules containing options to include
+    @return               a derivation that builds an options.json file
+    */
+    mkOptionsListFromHMSource = {
+      pkgs,
+      home-manager,
+      modules ? [],
+    }: let
+      hmLib = import "${home-manager}/modules/lib/stdlib-extended.nix" lib;
+
+      hmModules = import "${home-manager}/modules/modules.nix" {
+        inherit pkgs;
+        lib = hmLib;
+        check = false;
+      };
+
+      scrubDerivations = prefixPath: attrs: let
+        scrubDerivation = name: value: let
+          pkgAttrName = prefixPath + "." + name;
+        in
+          if lib.isAttrs value
+          then
+            scrubDerivations pkgAttrName value
+            // lib.optionalAttrs (lib.isDerivation value) {
+              outPath = "\${${pkgAttrName}}";
+            }
+          else value;
+      in
+        lib.mapAttrs scrubDerivation attrs;
+
+      # Make sure the used package is scrubbed to avoid actually
+      # instantiating derivations.
+      scrubbedPkgsModule = {
+        imports = [
+          {
+            _module.args = {
+              pkgs = lib.mkForce (scrubDerivations "pkgs" pkgs);
+              pkgs_i686 = lib.mkForce {};
+            };
+          }
+        ];
+      };
+
+      allModules =
+        hmModules
+        ++ modules
+        ++ [
+          scrubbedPkgsModule
+        ];
+
+      options =
+        (hmLib.evalModules {
+          modules = allModules;
+          class = "homeManager";
+        }).options;
+    in
+      mkOptionsList {
+        inherit options pkgs;
+      };
+  };
 in {
   inherit
     removeAtPath
     removeNestedAttrs
     mkOptionsList
     mkOptionsListFromModules
+    hm
     ;
 }

a65de335f1187a8607a7db3c4b5ec8b2672bd022
Varun Narravula <varun@snare.dev>
2025-07-02 15:06:51 -0700

    feat(module): add default.nix entrypoints for legacy module imports

--- default.nix
+++ default.nix
@@ -1,13 +1,22 @@
-(
-  import
-  (
-    let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    in
-      fetchTarball {
-        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-        sha256 = lock.nodes.flake-compat.locked.narHash;
-      }
-  )
-  {src = ./.;}
-).defaultNix
+{pkgs ? import <nixpkgs> {}}: let
+  flakeSelf =
+    (
+      import
+      (
+        let
+          lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+        in
+          fetchTarball {
+            url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+            sha256 = lock.nodes.flake-compat.locked.narHash;
+          }
+      )
+      {src = ./.;}
+    ).outputs;
+in {
+  inherit (flakeSelf.packages.${pkgs.system}) optnix;
+
+  nixosModules.optnix = flakeSelf.nixosModules.optnix;
+  darwinModules.optnix = flakeSelf.darwinModules.optnix;
+  homeModules.optnix = flakeSelf.homeModules.optnix;
+}

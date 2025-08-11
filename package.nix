c6585531607acb32496ec59b35aca88f87210faf
Varun Narravula <varun@snare.dev>
2025-08-11 01:48:41 -0700

    chore: reintroduce -dev suffix

--- package.nix
+++ package.nix
@@ -8,7 +8,7 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "optnix";
-  version = "0.3.0";
+  version = "0.3.0-dev";
   src = nix-gitignore.gitignoreSource [] ./.;
 
   vendorHash = "sha256-g/H91PiHWSRRQOkaobw2wAYX/07DFxWTCTlKzf7BT1Y=";
